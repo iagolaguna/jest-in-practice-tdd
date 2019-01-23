7449d10aa8fb3d5116f64e59cb5968bc2a9f275f
iagolaguna <iagolaguna.83@gmail.com>
2019-01-23 14:55:50 -0200

    finish reduce recursive

--- src/reduce/reduce-recursive.js
+++ src/reduce/reduce-recursive.js
@@ -1,3 +1,11 @@
-const reduce = () => { }
+const reduce = (arr, func, acc) => {
+    return (function reduceInternal (counter, acc, arrInternal) {
+        const [head, ...tail] = arrInternal;
+        return arrInternal.length
+            ? reduceInternal(counter + 1, func(acc, head, counter, arr), tail)
+            : acc
+    })(0, acc, arr)
+}
+
 
 export default reduce
